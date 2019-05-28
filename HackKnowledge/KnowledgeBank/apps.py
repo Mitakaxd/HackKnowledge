3319f9e77660fc1d90aaf58f7b102edf4531aa6b
Vladislav Spasov <wladislawspassow@gmail.com>
2019-05-29 00:36:16 +0300

    Add Home Page

---
+++ HackKnowledge/KnowledgeBank/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KnowledgebankConfig(AppConfig):
+    name = 'KnowledgeBank'
---
+++ HackKnowledge/KnowledgeBank/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class BaseUser:
+    pass
+
+class User:
+    pass
+# Create your models here.
+
+
+
+class Business:
+    pass
+
+
+class Course:
+    pass
