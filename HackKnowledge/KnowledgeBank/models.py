2806c5d8d545138afe748d29dc282bd7d619323b
Mitakaxd <mitaka.g.g@abv.bg>
2019-06-01 20:36:23 +0300

    better model

--- HackKnowledge/KnowledgeBank/models.py
+++ HackKnowledge/KnowledgeBank/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class BaseUser(models):
+class BaseUser(models.Model):
     user_name = models.CharField(max_length = 30)
     password = models.CharField(max_length = 20)
     country = models.CharField(max_length = 20)
